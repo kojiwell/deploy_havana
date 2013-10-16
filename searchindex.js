17a9ace199afc0d2b21b8ecae48163519401411d
Koji Tanaka <kj.tanaka@gmail.com>
2013-10-16 15:09:37 -0400

    updating the github pages

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,havana:0,welcom:0,deploy:0,modul:0,indic:0,content:0,tabl:0,openstack:0,document:0,simpl:0,page:0},objtypes:{},titles:["Welcome to Simple Deployment of OpenStack Havana&#8217;s documentation!"],objnames:{},filenames:["index"]})
