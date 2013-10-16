1f5c1fdb45ae5c46a9ea4711bfe3260e9bbca5ab
Koji Tanaka <kj.tanaka@gmail.com>
2013-10-16 15:45:38 -0400

    updating the github pages

--- _sources/add_compute.txt
+++ _sources/add_compute.txt
@@ -3,14 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Simple Deploy OpenStack Havana
+Add Compute Node
 ==========================================================
 
-Contents:
-
-.. toctree::
-   :maxdepth: 2
-
-   all_in_one
-   add_compute
-
+This is a test.test.
--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{node:[0,1],all:[0,1],havana:0,content:0,build:[0,1],thi:1,deploi:0,test:1,openstack:0,simpl:0},objtypes:{},titles:["Simple Deploy OpenStack Havana","Build All-in-One Node"],objnames:{},filenames:["index","all_in_one"]})
+Search.setIndex({objects:{},terms:{node:[0,1,2],all:[0,1],havana:0,comput:[0,2],content:0,add:[0,2],build:[0,1],thi:[1,2],deploi:0,test:[1,2],openstack:0,simpl:0},objtypes:{},titles:["Simple Deploy OpenStack Havana","Build All-in-One Node","Add Compute Node"],objnames:{},filenames:["index","all_in_one","add_compute"]})
