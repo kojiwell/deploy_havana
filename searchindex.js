98ab16fe9b88221689468469610286b4a80f4391
Koji Tanaka <kj.tanaka@gmail.com>
2013-10-16 15:30:13 -0400

    updating the github pages

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{node:1,all:1,havana:0,content:0,build:1,thi:1,deploi:0,test:1,openstack:0,simpl:0},objtypes:{},titles:["Simple Deploy OpenStack Havana","Build All-in-One Node"],objnames:{},filenames:["index","all_in_one"]})
+Search.setIndex({objects:{},terms:{node:[0,1],all:[0,1],havana:0,content:0,build:[0,1],thi:1,deploi:0,test:1,openstack:0,simpl:0},objtypes:{},titles:["Simple Deploy OpenStack Havana","Build All-in-One Node"],objnames:{},filenames:["index","all_in_one"]})
